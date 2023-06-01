a92c9d130f24467c740bc1aa74c4f1f9861723b7
Kiet <kietquang240703@gmail.com>
2023-06-01 15:43:14 +0700

    Latest version lab1

--- src/shared/ListofFilms.js
+++ src/shared/ListofFilms.js
@@ -1,10 +1,10 @@
 export const Films=[
     {id: 1, img: 'images/Star Wars.jpg', Title: 'Star Wars: Episode IV - A New Hope', Year:'1977', Nation: 'USA' },
-    {id: 2, img: 'images/Harry -Potter.jpg', Title: 'Harry potter and the secret chamber', Year:'2002', Nation: 'Germany' },
-    {id: 3, img: 'images/avengers.jpg', Title: 'Avengers Endgame', Year:'2019', Nation: 'London' },
+    {id: 2, img: 'images/Harry -Potter.jpg', Title: 'Harry Potter and the Philosopher Stone', Year:'2002', Nation: 'Germany' },
+    {id: 3, img: 'images/Captain.jpg', Title: 'Captain America: The Winter Soldier', Year:'2014', Nation: 'USA' },
     {id: 4, img: 'images/F&F 10.jpg', Title: 'Fast & Furious 10', Year:'2023', Nation: 'USA' },
     {id: 5, img: 'images/Phineas&Ferb.jpg', Title: 'Phineas And Ferb', Year:'2007', Nation: 'USA' },
-    {id: 6, img: 'images/Spider man.jpg', Title: 'Spider-Man: No Way Home', Year:'2021', Nation: 'USA' },
+    {id: 6, img: 'images/Spider man.jpg', Title: 'Spider-Man', Year:'2021', Nation: 'USA' },
     {id: 7, img: 'images/Alvin Chipmunks.jpg', Title: 'The Chipmunks', Year:'2013', Nation: 'USA' },
     {id: 8, img: 'images/The lion king.jpg', Title: 'The Lion King', Year:'2021', Nation: 'USA' },
     {id: 9, img: 'images/wakanda.jpg', Title: 'Wakanda Forever', Year:'2022', Nation: 'USA' },
